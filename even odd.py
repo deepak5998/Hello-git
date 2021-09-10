ca3de882a1567d47c184c490b0819a64e74a42d3
deepak5998 <deepakkumbhar1920@gmail.com>
2021-09-10 17:52:10 +0530

    initial commit

---
+++ even odd.py
@@ -0,0 +1,63 @@
+#even odd program
+'''while (1):
+ a = int(input('input number'))
+ if (a%2 ==0):print('even')
+ else:print('odd')
+
+
+#leap year program
+a = int (input( 'input year'))
+if (a%400==0)
+    print('a is a leap year')
+if a%4==0 and a%100 !=0
+    print(' a is nat a leap year')
+else:print('not a leap year')
+
+#add two numbers
+a,b=int(input('enter number 1')),int(input('enter number 2'))
+s=a+b
+print('the sum is',s)
+
+#remove duplicate  from ist
+l = []
+n=int (input('how many names to enter'))
+for i in range(n) :
+    print(i+1,'enter names')
+    l.append(input())
+s=set(l)
+l=list(s)
+for x in l:
+    print(x)
+
+#print users first n natural numbers
+n=int(input('how many number to proint'))
+for i in range(n):
+    print(i+1)
+
+
+#printing numbers in reverse order.
+l=[]
+for x in range(5):
+    print(x+1 ,'enter numbers')
+    l.append(input())
+
+l.reverse()
+print(l)
+
+#print numbers in reverse order
+
+for x in range(9,3,-1):
+    print(x,end='')
+
+i=10
+while i>1 :
+    i-=1
+    print(i+1,end='')
+    '''
+
+
+
+
+
+
+
